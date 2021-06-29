6ac7d2ddcdfae857dd770ca4b7c0cee68a01857e
Jim Drury <jim@jimdrury.co.uk>
2021-06-29 20:59:04 +0100

    feat(next): configuring for serverless

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,10 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
 
-module.exports = withNx({
+const nextConfig = {
+  target: 'experimental-serverless-trace',
   nx: {
-    // Set this to false if you do not want to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: true,
-  },
-});
+    svgr: true
+  }
+};
+
+module.exports = withNx(nextConfig);
