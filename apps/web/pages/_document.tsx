f80cdc1b1616213f8c47b1c99b5ef01734ff3df9
Jim Drury <jim@jimdrury.co.uk>
2021-06-30 18:21:26 +0100

    feat(doc): adds language

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -19,7 +19,7 @@ export default class CustomDocument extends Document<{
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>{this.props.styleTags}</Head>
         <body>
           <Main />
