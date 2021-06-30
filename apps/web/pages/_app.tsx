f3ec4f9f69a3baadd73866d910293d26e5de2925
Jim Drury <jim@jimdrury.co.uk>
2021-06-30 17:21:24 +0100

    feat(fonts): adds josefin sans

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -8,14 +8,17 @@ function CustomApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Welcome to web!</title>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin="true" />
+        <link href='https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap' rel='stylesheet'/>
       </Head>
-      <div className="app">
+      <div className='app'>
         <main>
           <Component {...pageProps} />
         </main>
       </div>
     </>
-  );
+);
 }
 
 export default CustomApp;
