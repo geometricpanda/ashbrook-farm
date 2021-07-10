ae192e205d7719f19a87b8302217e2afb7ac31a1
Jim Drury <jim@jimdrury.co.uk>
2021-07-10 11:29:14 +0100

    fix(header): moving outside of pages folder

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
-import { Header } from './common/header';
+import { Header } from '../common/header';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
